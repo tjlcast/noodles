1be90dac72be8d180252508796f6d0a4852f1004
tangjialiang <phx108@sina.com>
2017-11-27 16:26:36 +0800

    basis build

--- application/src/main/java/com/tjlcast/app/App.java
+++ application/src/main/java/com/tjlcast/app/App.java
@@ -1,7 +1,19 @@
 package com.tjlcast.app;
 
+import com.tjlcast.ActorSystemContext;
+import com.tjlcast.service.ContextAwareActor;
+
 /**
  * Created by tangjialiang on 2017/11/27.
+ *
  */
-public class App {
+
+public class App extends ContextAwareActor{
+    public App(ActorSystemContext systemContext) {
+        super(systemContext);
+    }
+
+    public void onReceive(Object message) throws Exception {
+
+    }
 }
